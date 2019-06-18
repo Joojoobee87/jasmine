665c34e41986557e829d157d7dc76bf78dfc4ecd
Joanne Broomfield <joanneingle1987@yahoo.co.uk>
2019-06-18 19:59:48 +0000

    final commit before migration

---
+++ spec/drinkSpec.js
@@ -0,0 +1,18 @@
+describe("my whatCanIDrink function", function() {
+
+    beforeEach(function() {
+        drink = new whatCanIDrink();
+    });
+
+    describe("Checks age", function() {
+        it("Should exist", function() {
+            expect(whatCanIDrink).toBeDefined();
+        })
+    })
+
+
+
+
+
+
+});
